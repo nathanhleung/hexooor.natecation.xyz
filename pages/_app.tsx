d0980393e466ae9b92c9d8ade470d9b80010436c
nathanhleung <github@nathanhleung.com>
2022-01-20 16:52:50 -0800

    Add Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import ReactGA from "react-ga";
 
 import "../styles/globals.css";
+
+ReactGA.initialize("G-4M24160CZH");
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
